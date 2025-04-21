395ba077e3a73fbb5e0c04fc9ce74c2c01ffdb78
Jesús González <5jegonza@gmail.com>
2025-04-21 10:33:10 +0000

    Chapter 2: Basics finished

--- Start/Ch2/functions_start.py
+++ Start/Ch2/functions_start.py
@@ -3,17 +3,47 @@
 
 
 # define a basic function
-print("hello world!")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+# def hello_func():
+#   print("hello world!")
+#   name = input("What is your name? ")
+#   print("Nice to meet you,", name)
+
+# hello_func()
 
 # function that takes parameters
+# def hello_func(greeting):
+#   print("hello world!")
+#   name = input("What is your name? ")
+#   print(greeting, name)
+
+# hello_func("How are you doing?")
+# hello_func("Hey, what's up?")
 
 
 # function that returns a value
+# def cube(x):
+#   return x*x*x
 
+# result = cube(3)
+# print(result)
 
 # function with default value for an parameter
+# def hello_func(greeting, name = None):
+#   print("hello world!")
+#   if name == None:
+#     name = input("What is your name? ")
+#   print(greeting, name)
+
+# hello_func("Nice to meet you", "Jesus")
+# hello_func("Nice to meet you")
+# hello_func(name = "Jesus", greeting = "Nice to meet you")
 
 
 # function with variable number of parameters
+def multi_add(start, *args):
+  result = start
+  for x in args:
+    result = result + x
+  return result
+
+print(multi_add(0, 4, 5, 10, 4))
